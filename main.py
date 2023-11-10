022af28efedc71e6ab2c9775a1167a1243061dfe
Zeyu Jiang <147691140+baojudezeze@users.noreply.github.com>
2023-11-10 10:34:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,112 +1,95 @@
+import argparse
+
+import numpy as np
+import pandas as pd
 import torch
 import torchvision
 import torchvision.transforms as transforms
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-import pickle
-import PIL
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import time
-from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from torch.utils.data import DataLoader
+
 import model
-import ResNet
+import pyramidnet
+import utils
 
 
-def get_CIFAR10_data():
-    train_transform = transforms.Compose(
-        [transforms.RandomHorizontalFlip(p=0.5),
-         transforms.RandomAffine(degrees=(-5, 5), translate=(0.1, 0.1), scale=(0.9, 1.1)),
-         transforms.ToTensor(),
-         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+def get_CIFAR10_data(batch_size, num_workers):
+    train_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transforms.ToTensor())
+    data = torch.cat([d[0] for d in DataLoader(train_set)])
 
-    test_transform = transforms.Compose(
-        [transforms.ToTensor(),
-         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    # data augmentation
+    train_transform = transforms.Compose([
+        torchvision.transforms.RandomCrop(size=(32, 32), padding=4),
+        torchvision.transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(data.mean(dim=[0, 2, 3]), data.std(dim=[0, 2, 3])),
+        utils.Cutout()
+    ])
 
-    dataset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=train_transform)
-    train_set, val_set = torch.utils.data.random_split(dataset, [45000, 5000])
+    val_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(data.mean(dim=[0, 2, 3]), data.std(dim=[0, 2, 3]))
+    ])
+
+    train_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=train_transform)
+    val_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=val_transform)
+
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=num_workers)
     val_loader = torch.utils.data.DataLoader(val_set, batch_size=batch_size, shuffle=False, num_workers=num_workers)
-    test_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=test_transform)
-    test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=num_workers)
-    classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-    return dataset, train_set, val_set, test_set, train_loader, val_loader, test_loader, classes
+    return train_set, val_set, train_loader, val_loader, classes
 
 
 def model_train():
-    loss_hist, acc_hist = [], []
-    loss_hist_val, acc_hist_val = [], []
+    for epoch in range(args.epochs):
 
-    for epoch in range(epochs):
-        time_ckpt = time.time()
-        print("EPOCH:", epoch + 1, end=" ")
-        running_loss = 0.0
-        correct = 0
+        model.train()
+        log.train(len_dataset=len(train_loader))
+
+        # train loss
         for data in train_loader:
             batch, labels = data
             batch, labels = batch.to(device), labels.to(device)
 
-            optimizer.zero_grad()
+            # the SAM Optimizer, which needs two forward-backward passes to estimate the "sharpness-aware" gradient
+            # step 1
+            utils.enable_running_stats(model)
             outputs = model(batch)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
+            loss = utils.smooth_crossentropy(outputs, labels, smoothing=args.label_smoothing)
+            loss.mean().backward()
+            optimizer.first_step(zero_grad=True)
 
-            # compute training statistics
-            _, predicted = torch.max(outputs, 1)
-            correct += (predicted == labels).sum().item()
-            running_loss += loss.item()
+            # step 2
+            utils.disable_running_stats(model)
+            utils.smooth_crossentropy(model(batch), labels, smoothing=args.label_smoothing).mean().backward()
+            optimizer.second_step(zero_grad=True)
 
-        avg_loss = running_loss / len(train_set)
-        avg_acc = correct / len(train_set)
-        loss_hist.append(avg_loss)
-        acc_hist.append(avg_acc)
+            with torch.no_grad():
+                correct = torch.argmax(outputs.data, 1) == labels
+                log(model, loss.cpu(), correct.cpu(), scheduler.lr())
+                scheduler(epoch)
 
-        # validation statistics
         model.eval()
+        log.eval(len_dataset=len(val_loader))
+
+        # val loss
         with torch.no_grad():
-            loss_val = 0.0
-            correct_val = 0
             for data in val_loader:
                 batch, labels = data
                 batch, labels = batch.to(device), labels.to(device)
                 outputs = model(batch)
-                loss = criterion(outputs, labels)
-                _, predicted = torch.max(outputs, 1)
-                correct_val += (predicted == labels).sum().item()
-                loss_val += loss.item()
-
-            avg_loss_val = loss_val / len(val_set)
-            avg_acc_val = correct_val / len(val_set)
-            loss_hist_val.append(avg_loss_val)
-            acc_hist_val.append(avg_acc_val)
-        model.train()
-        scheduler.step(avg_loss_val)
-
-        print("Training Loss: {:.3f}".format(avg_loss * 100), end=" ")
-        print("Val Loss: {:.3f}".format(avg_loss_val * 100), end=" ")
-        print("Train Accuracy: {:.2f}%".format(avg_acc * 100), end=" ")
-        print("Val Accuracy: {:.2f}%".format(avg_acc_val * 100), end=" ")
-
-        with open("Models/cnn_cifar10_model_{}.pth".format(epoch + 1), "wb") as f:
-            model.eval()
-            pickle.dump(model, f)
-            model.train()
-        print("Time: {:.2f} sec".format(time.time() - time_ckpt), end=" \n")
-
-    return loss_hist, loss_hist_val, acc_hist, acc_hist_val
+                loss = utils.smooth_crossentropy(outputs, labels)
+                correct = torch.argmax(outputs, 1) == labels
+                log(model, loss.cpu(), correct.cpu())
+    log.flush()
 
 
 def complementary_show(show: bool):
     if show:
         # create confusion matrix
         confusion_mat = confusion_matrix(label_vec, pred_vec)
-        labels = np.unique(label_vec)
         confusion_df = pd.DataFrame(confusion_mat, index=classes, columns=classes)
         print("Confusion Matrix")
         print(confusion_df)
@@ -120,86 +103,81 @@ def complementary_show(show: bool):
 
 if __name__ == '__main__':
     # hyperparameters
-    epochs = 80
-    learning_rate = 0.01
-    batch_size = 128
-    num_workers = 2
-    # model = model.Net()
-    # model = torchvision.models.resnet50(pretrained=False)
-    model = ResNet.ResNet18()
-    comp_show = True
-    # 基础准确率：76.28%
-    # 50resnet 82.66%
-    # Resnet 90.66%
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--batch_size", default=128, type=int)
+    parser.add_argument("--depth", default=28, type=int)
+    parser.add_argument("--dropout", default=0, type=float)
+    parser.add_argument("--epochs", default=300, type=int)
+    parser.add_argument("--label_smoothing", default=0.1, type=float, help="Use 0.1 for label smoothing.")
+    parser.add_argument("--learning_rate", default=0.1, type=float)
+    parser.add_argument("--momentum", default=0.9, type=float, help="SGD Momentum.")
+    parser.add_argument("--rho", default=2.0, type=int, help="Rho parameter for SAM.")
+    parser.add_argument("--weight_decay", default=0.0005, type=float)
+    parser.add_argument("--width_factor", default=10, type=int, help="How many times wider compared to normal ResNet.")
+    parser.add_argument("--num_workers", default=2, type=int)
+    parser.add_argument("--comp_show", default=True, type=bool)
+    args = parser.parse_args()
 
-    # device
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model.to(device)
+    # cudnn settings
+    utils.initialize(seed=42)
+
+    # device and model
+    device = torch.device("cuda")
+    model = model.WideResNet(args.depth, args.width_factor, dropout=args.dropout, in_channels=3, labels=10).to(device)
+    # model = model.PyramidNet(dataset='cifar10', depth=110, alpha=64, num_classes=10, bottleneck=False)
+    # model = pyramidnet.PyramidNet(dataset='cifar10', depth=272, alpha=48, num_classes=10, bottleneck=True).cuda()
 
     # get dataset
-    dataset, train_set, val_set, test_set, train_loader, val_loader, test_loader, classes = get_CIFAR10_data()
+    train_set, val_set, train_loader, val_loader, classes = get_CIFAR10_data(args.batch_size, args.num_workers)
+
+    # log
+    log = utils.Log(log_each=10)
 
     # record parameters
     num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
     print("Number of trainable parameters:", num_params)
 
-    # optimizer
-    criterion = nn.CrossEntropyLoss()
-    optimizer = \
-        optim.Adam(model.parameters(), lr=learning_rate, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False)
-    scheduler = \
-        optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10, verbose=True, min_lr=0)
+    # optimizer and scheduler
+    optimizer = utils.SAM(model.parameters(), torch.optim.SGD, rho=args.rho, adaptive=True,
+                          lr=args.learning_rate,
+                          momentum=args.momentum, weight_decay=args.weight_decay)
+    scheduler = utils.StepLR(optimizer, args.learning_rate, args.epochs)
 
     # train model
-    loss_hist, loss_hist_val, acc_hist, acc_hist_val = model_train()
-
-    # loss visualization
-    plots = [(loss_hist, loss_hist_val), (acc_hist, acc_hist_val)]
-    plt_labels = [("Training Loss", "Validation Loss"), ("Training Accuracy", "Validation Accuracy")]
-    plt_titles = ["Loss", "Accuracy"]
-    plt.figure(figsize=(20, 7))
-    for i in range(0, 2):
-        ax = plt.subplot(1, 2, i + 1)
-        ax.plot(plots[i][0], label=plt_labels[i][0])
-        ax.plot(plots[i][1], label=plt_labels[i][1])
-        ax.set_title(plt_titles[i])
-        ax.legend()
-    plt.show()
-
-    # selecting the best model and epoch
-    best_acc = max(acc_hist_val)
-    best_epoch = acc_hist_val.index(best_acc) + 1
-    print("Best accuracy on validation set: {:.2f}%".format(best_acc * 100))
-    print("Best epoch: {}".format(best_epoch))
-    with open(f"Models/cnn_cifar10_model_{best_epoch}.pth", "rb") as f:
-        loaded_model = pickle.load(f)
+    model_train()
 
     # test the Trained Network
+    device = torch.device("cuda")
+    test_transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+         ])
     pred_vec = []
     label_vec = []
     correct = 0
     test_loss = 0.0
     model.to(device)
+    test_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=test_transform)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=128, shuffle=False, num_workers=2)
     with torch.no_grad():
         for data in test_loader:
             batch, labels = data
             batch, labels = batch.to(device), labels.to(device)
             outputs = model(batch)
-            test_loss = criterion(outputs, labels)
+            test_loss = utils.smooth_crossentropy(outputs, labels)
             _, predicted = torch.max(outputs, 1)
             correct += (predicted == labels).sum().item()
-            pred_vec.extend(predicted.cpu().numpy())  # Convert tensor to numpy array
-            label_vec.extend(labels.cpu().numpy())  # Convert tensor to numpy array
+            pred_vec.extend(predicted.cpu().numpy())
+            label_vec.extend(labels.cpu().numpy())
     pred_vec = np.array(pred_vec)
     label_vec = np.array(label_vec)
-    print("Test Loss: {:.2f}".format(test_loss))
+    test_loss = np.array(test_loss.cpu())
+    print("Test Loss: {:.2f}".format(test_loss[-1]))
     print('Test Accuracy on the 10000 test images: %.2f %%' % (100 * correct / len(test_set)))
 
+    # best 96.93 % wrn epoch120
+    # best 97.27 % wrn epoch200
+    # best 96.26% pyramid epoch200
+
     # compute the Accuracy, F1-Score, Precision, Recall, Support
-    complementary_show(comp_show)
-
-
-
-
-
-
+    complementary_show(args.comp_show)
